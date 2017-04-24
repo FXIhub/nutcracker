d0fc0f4b4f3b5b3c4d7c61428d42f17a14bf8427
Louis Doctor <doctor@login.davinci>
2017-04-24 10:18:29 +0200

    added FSC test function

---
+++ nutcracker/tests/test_intensities.py
@@ -0,0 +1,15 @@
+import h5py
+import numpy as np
+import nutcracker
+
+
+def test_fourier_shell_correlation():
+    with h5py.File('/scratch/fhgfs/doctor/1FFK/models/1FFK_model_hc_fs.h5', "r") as f:
+        img = f['real']
+
+    fsc = nutcracker.fourier_shell_correlation(img,img):
+
+    if fsc.mean() == 1.0:
+        print 'passed'
+    else:
+        print 'failed'
