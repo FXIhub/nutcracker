0e3d9c37e335f7f7274e539208a1438220df6090
Louis Doctor <doctor@xray.bmc.uu.se>
2017-05-02 13:54:52 +0200

    small change

--- nutcracker/tests/test_rotate.py
+++ nutcracker/tests/test_rotate.py
@@ -3,17 +3,13 @@ import numpy as np
 import nutcracker
 import unittest
 
+with h5py.File('/home/doctor/software/nutcracker/nutcracker/data/test_data.h5', 'r') as f:
+    img_1 = f['real'][:]
+with h5py.File('/home/doctor/software/nutcracker/nutcracker/data/test_data_rot_shift.h5', 'r') as f:
+    img_2 = f['real'][:]
 
 class TestCaseRotate(unittest.TestCase):
     def test_find_rotation_between_two_models(self):
-        with h5py.File('/home/doctor/software/nutcracker/nutcracker/data/test_data.h5', 'r') as f:
-            img_1 = f['real']
-        with h5py.File('/home/doctor/software/nutcracker/nutcracker/data/test_data_rot_shift.h5', 'r') as f:
-            img_2 = f['real']
-            
-        #img_1 = test_data[0]
-        #img_2 = test_data[1]
-
         Img_1 = np.abs(np.fft.fftshift(np.fft.fftn(img_1)))**2
         Img_2 = np.abs(np.fft.fftshift(np.fft.fftn(img_2)))**2
         
@@ -26,15 +22,11 @@ class TestCaseRotate(unittest.TestCase):
         self.assertTrue(np.alltrue(np.round(out_calculated-out_expected,2) == 0))
 
     def test_find_shift_between_two_models(self):
-        with h5py.File('/home/doctor/software/nutcracker/nutcracker/data/test_data.h5', 'r') as f:
-            img_1 = f['real']
-        with h5py.File('/home/doctor/software/nutcracker/nutcracker/data/test_data_rot_shift.h5', 'r')as f:
-            img_2 = f['real']
-            
-        #img_1 = test_data[0]
-        #img_2 = test_data[1]
-        
         out_calculated = nutcracker.utils.rotate.find_shift_between_two_models(img_2,img_1)
         out_expected = np.array((2,-3,1))
         
         self.assertTrue(np.alltrue(np.round(out_calculated-out_expected) == 0))
+
+    
+#    def test_rotation_based_on_euler_angles(self):
+        
