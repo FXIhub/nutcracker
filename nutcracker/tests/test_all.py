176054b3b42c8ec905d3ea4b8fd5f7e50777419c
Louis Doctor <doctor@xray.bmc.uu.se>
2017-05-29 15:17:03 +0200

    added plot analysis test

--- nutcracker/tests/test_all.py
+++ nutcracker/tests/test_all.py
@@ -6,6 +6,7 @@ from test_quaternions import TestCaseQuaternions
 from test_real_space import TestCaseRealSpace
 from test_shift import TestCaseShift
 from test_error_matrix import TestCaseErrorMatrix
+from test_plot_analysis import TestCasePlotAnalysis
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
--- nutcracker/utils/plot_analysis.py
+++ nutcracker/utils/plot_analysis.py
@@ -1,7 +1,7 @@
 import numpy as np
+import scipy as sp
 
-
-def envelope(function,frequence,peak_finding_threshold=None):
+def envelope(function,frequence,peak_finding_threshold=None,order_spline_interpolation=3):
     """
     Calculates the upper and lower envelope of a given function.
     This function is based on https://stackoverflow.com/questions/34235530/python-how-to-get-high-and-low-envelope-of-a-signal (24.05.2017)
@@ -12,6 +12,7 @@ def envelope(function,frequence,peak_finding_threshold=None):
 
     Kwargs:
         :peak_finding_threshold(float):   threshold of peak finder (raion between maximum and minimum), peaks would then not take into account, default=None
+        :order_spline_interpolation(int): order of the spline interpolation
     """
 
     upper_envelope_x = []
@@ -40,7 +41,7 @@ def envelope(function,frequence,peak_finding_threshold=None):
                 upper_envelope_y[k] = np.mean([upper_envelope_y[k-2],upper_envelope_y[k-1],upper_envelope_y[k+1],upper_envelope_y[k+2]])
                 upper_envelope_y[k] = np.mean([lower_envelope_y[k-2],lower_envelope_y[k-1],lower_envelope_y[k+1],lower_envelope_y[k+2]])
 
-    upper_envelope = scipy.interpolate.interp1d(upper_envelope_x,upper_envelope_y, kind = 'cubic',bounds_error = False, fill_value=0.0)
-    lower_envelope = scipy.interpolate.interp1d(lower_envelope_x,lower_envelope_y, kind = 'cubic',bounds_error = False, fill_value=0.0)
+    upper_envelope = sp.interpolate.interp1d(upper_envelope_x,upper_envelope_y, kind = order_spline_interpolation,bounds_error = False, fill_value=0.0)
+    lower_envelope = sp.interpolate.interp1d(lower_envelope_x,lower_envelope_y, kind = order_spline_interpolation,bounds_error = False, fill_value=0.0)
 
     return upper_envelope, lower_envelope
