ce9c22a600cdde61e9dfbd2bc6cc0c037dffa9e9
Louis Doctor <doctor@xray.bmc.uu.se>
2017-05-30 15:03:08 +0200

    fixed bugs and peak finding

--- nutcracker/utils/plot_analysis.py
+++ nutcracker/utils/plot_analysis.py
@@ -1,7 +1,7 @@
 import numpy as np
 import scipy as sp
 
-def envelope(function,frequence,peak_finding_threshold=None,order_spline_interpolation=3):
+def envelope(function,frequence,peak_finding_threshold=(None,None),order_spline_interpolation=3):
     """
     Calculates the upper and lower envelope of a given function.
     This function is based on https://stackoverflow.com/questions/34235530/python-how-to-get-high-and-low-envelope-of-a-signal (24.05.2017)
@@ -11,16 +11,18 @@ def envelope(function,frequence,peak_finding_threshold=None,order_spline_interpo
         :frequence(int):                  frequence at which function should be sample
 
     Kwargs:
-        :peak_finding_threshold(float):   threshold of peak finder (raion between maximum and minimum), peaks would then not take into account, default=None
+        :peak_finding_threshold(list):    threshold of peak finder given as tuple (min,max), peaks would then not take into account, default=(None,None)
         :order_spline_interpolation(int): order of the spline interpolation
     """
 
+    # creating lists for envelope
     upper_envelope_x = []
     upper_envelope_y = []
 
     lower_envelope_x = []
     lower_envelope_y = []
 
+    # iterating through function at given frequency
     for k in np.arange(0,len(function), frequence):
         part = function[k:k+frequence]
 
@@ -30,18 +32,31 @@ def envelope(function,frequence,peak_finding_threshold=None,order_spline_interpo
         lower_envelope_x.append(k)
         lower_envelope_y.append(part.min())
 
+    # transform to numpy arrays
     upper_envelope_x = np.array(upper_envelope_x)
     upper_envelope_y = np.array(upper_envelope_y)
     lower_envelope_x = np.array(lower_envelope_x)
     lower_envelope_y = np.array(lower_envelope_y)
 
-    if peak_finding_threshold:
-        for k in np.arange(0,len(function), frequence):
-            if (upper_envelope_y[k] / lower_envelope_y[k]) >= peak_finding_threshold:
-                upper_envelope_y[k] = np.mean([upper_envelope_y[k-2],upper_envelope_y[k-1],upper_envelope_y[k+1],upper_envelope_y[k+2]])
-                upper_envelope_y[k] = np.mean([lower_envelope_y[k-2],lower_envelope_y[k-1],lower_envelope_y[k+1],lower_envelope_y[k+2]])
+    # peak finding lower envelope
+    if peak_finding_threshold[0]:
+        for k in np.arange(len(lower_envelope_y)):
+            if lower_envelope_y[k] < peak_finding_threshold[0]:
+                lower_envelope_y[k] = np.mean([lower_envelope_y[k-2],lower_envelope_y[k-1]])
 
+    # peak finding upper envelope
+    if peak_finding_threshold[1]:
+        for k in np.arange(len(upper_envelope_y)):
+            if upper_envelope_y[k] > peak_finding_threshold[1]:
+                upper_envelope_y[k] = np.mean([upper_envelope_y[k-2],upper_envelope_y[k-1]])
+
+    # interpolate the envelope function
     upper_envelope = sp.interpolate.interp1d(upper_envelope_x,upper_envelope_y, kind = order_spline_interpolation,bounds_error = False, fill_value=0.0)
     lower_envelope = sp.interpolate.interp1d(lower_envelope_x,lower_envelope_y, kind = order_spline_interpolation,bounds_error = False, fill_value=0.0)
+
+    x = np.arange(len(function))
+
+    upper_envelope = upper_envelope(x)
+    lower_envelope = lower_envelope(x)
 
     return upper_envelope, lower_envelope
