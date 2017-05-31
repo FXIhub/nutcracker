093aabcb394db952dfa505c64d5ec8875448ab32
Louis Doctor <doctor@xray.bmc.uu.se>
2017-05-31 10:18:30 +0200

    fixed bug

--- nutcracker/tests/test_plot_analysis.py
+++ nutcracker/tests/test_plot_analysis.py
@@ -7,7 +7,7 @@ class TestCasePlotAnalysis(unittest.TestCase):
         x = np.arange(100)
         y = np.sinc(x * 0.05)
 
-        upper, lower = nutcracker.utils.plot_analysis.envelope(y,1,order_spline_interpolation=3,peak_finding_threshold=(1,1))
+        upper, lower = nutcracker.utils.plot_analysis.envelope(y,1,order_spline_interpolation=3,peak_finding_threshold=(0,1))
 
         self.assertTrue(np.alltrue(np.round(y - upper,7) == 0) and np.alltrue(np.round(y - lower,7) == 0))
         
